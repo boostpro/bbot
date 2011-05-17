b2032703b8086edf0b504d429726dea14949e621
Dave Abrahams <dave@boostpro.com>
2011-05-17 15:01:52 -0600

    Break out emacs procedures

---
+++ emacs.py
@@ -0,0 +1,38 @@
+from procedures import BuildProcedure
+from buildbot.steps.source import Git
+from buildbot.steps.shell import Test, SetProperty
+from buildbot.steps.slave import SetPropertiesFromEnv
+from buildbot.process.properties import WithProperties
+
+def Emacs():
+    return WithProperties(
+        '%(EMACS)s'
+        , EMACS=lambda props: props.getProperty('EMACS','emacs')
+        )
+
+def EmacsTest(*args, **kw):
+    return Test(
+        command=[Emacs(), '--no-splash', '--debug-init'] + (
+            list(args)
+            + reduce(lambda r, kv: r+['--'+kv[0],kv[1]], kw.items(), [])),
+        env = { 'HOME': WithProperties('%(FakeHome)s') },
+        timeout = kw.get('timeout', 40),
+        logfiles = dict(testlog=dict(filename='test.log'))
+        )
+
+class GitHubElisp(BuildProcedure):
+    def __init__(self, repo, *testnames):
+        BuildProcedure.__init__(self, 'elisp')
+        self.addSteps(
+            Git(repourl='git://github.com/%s.git' % repo),
+            SetPropertiesFromEnv(variables=['EMACS']),
+            SetProperty(
+                command=[Emacs(), '--batch', '--eval', 
+                         '(princ (make-temp-file "home" t ".bbot"))'],
+                extract_fn=lambda rc, stdout, stderr: dict(FakeHome=stdout)
+                ))
+
+        for t in testnames or ['test/test']:
+            self.addStep(EmacsTest(load= t+'.el'))
+
+
--- procedures.py
+++ procedures.py
@@ -1,8 +1,4 @@
 from buildbot.process import factory
-from buildbot.steps.source import Git
-from buildbot.steps.shell import Test, SetProperty
-from buildbot.steps.slave import SetPropertiesFromEnv
-from buildbot.process.properties import WithProperties
 
 class BuildProcedure(factory.BuildFactory):
     """
@@ -26,38 +22,6 @@ class BuildProcedure(factory.BuildFactory):
 
     def __repr__(self):
         return self.__class__.__name__ + '(' + repr(self.name) + ')' 
-
-def Emacs():
-    return WithProperties(
-        '%(EMACS)s'
-        , EMACS=lambda props: props.getProperty('EMACS','emacs')
-        )
-
-def EmacsTest(*args, **kw):
-    return Test(
-        command=[Emacs(), '--no-splash', '--debug-init'] + (
-            list(args)
-            + reduce(lambda r, kv: r+['--'+kv[0],kv[1]], kw.items(), [])),
-        env = { 'HOME': WithProperties('%(FakeHome)s') },
-        timeout = kw.get('timeout', 40),
-        logfiles = dict(testlog=dict(filename='test.log'))
-        )
-
-class GitHubElisp(BuildProcedure):
-    def __init__(self, repo, *testnames):
-        BuildProcedure.__init__(self, 'elisp')
-        self.addSteps(
-            Git(repourl='git://github.com/%s.git' % repo),
-            SetPropertiesFromEnv(variables=['EMACS']),
-            SetProperty(
-                command=[Emacs(), '--batch', '--eval', 
-                         '(princ (make-temp-file "home" t ".bbot"))'],
-                extract_fn=lambda rc, stdout, stderr: dict(FakeHome=stdout)
-                ))
-
-        for t in testnames or ['test/test']:
-            self.addStep(EmacsTest(load= t+'.el'))
-
 
 if __name__ == '__main__':
     import doctest
