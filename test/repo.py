ac13b3c0511538e2736104d2c70bb97da9881ad9
Dave Abrahams <dave@boostpro.com>
2011-07-25 21:52:26 -0400

    Move TempDir into a module

--- test/repo.py
+++ test/repo.py
@@ -1,38 +1,7 @@
-import tempfile
-import shutil
+import tempdir
 import quiet_process
-import os
-import path
 
-class TempDir(object):
-    path = None
-    def __init__(self, *args, **kw):
-        '''
-        >>> d = TempDir()
-        >>> os.path.isdir(d.path)
-        True
-        '''
-        self.path = path.Path(tempfile.mkdtemp(*args, **kw))
-
-    def __str__(self):
-        return self.path
-        
-    def __del__(self):
-        '''
-        >>> d = TempDir()
-        >>> p = d.path
-        >>> f = p / 'foo'
-        >>> _ = open(f, 'w')
-        >>> os.path.exists(f)
-        True
-        >>> del d
-        >>> os.path.exists(p)
-        False
-        '''
-        if self.path:
-            shutil.rmtree(self.path, ignore_errors=True)
-
-class LocalGit(TempDir):
+class LocalGit(tempdir.TempDir):
     def __init__(self, *args, **kw):
         '''
         >>> r = LocalGit()
@@ -56,7 +25,7 @@ class LocalGit(TempDir):
         0
         '''
         quiet_process.check_call(['git','add', relpath], cwd=self.path)
-
+        
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
