aac8a1b302261cbbe8df403291a58fb478b3e2d2
Dave Abrahams <dave@boostpro.com>
2011-07-25 22:15:46 -0400

    Add test for LocalGit.check_call and use it in other tests

--- test/repo.py
+++ test/repo.py
@@ -1,11 +1,11 @@
 import tempdir
-import quiet_process
+import quiet_process as quietly
 
 class LocalGit(tempdir.TempDir):
     def __init__(self, *args, **kw):
         '''
         >>> r = LocalGit()
-        >>> quiet_process.call(['git','status'], cwd=r.path)
+        >>> quietly.call(['git','status'], cwd=r.path)
         0
         '''
         super(LocalGit, self).__init__(*args, **kw)
@@ -13,21 +13,37 @@ class LocalGit(tempdir.TempDir):
 
     def add(self, relpath='.'):
         '''
-        >>> from quiet_process import *
         >>> r = LocalGit()
         >>> open(r.path/'README','w').close()
         >>> r.add('README')
-        >>> call(['git','rev-parse', 'HEAD'], cwd=r.path) == 0
+        >>> quietly.call(['git','rev-parse', 'HEAD'], cwd=r.path) == 0
         False
-        >>> check_call(['git','commit', '-m', 'comment'], cwd=r.path)
+        >>> r.check_call(['git','commit', '-m', 'comment'])
         0
-        >>> check_call(['git','rev-parse', 'HEAD'], cwd=r.path)
+        >>> r.check_call(['git','rev-parse', 'HEAD'])
         0
         '''
         self.check_call(['git','add', relpath])
         
+    def commit(self, msg='Empty commit message'):
+        '''
+        >>> r = LocalGit()
+        >>> open(r.path/'README','w').close()
+        >>> r.add('README')
+        >>> quietly.call(['git','rev-parse', 'HEAD'], cwd=r.path) == 0
+        False
+        >>> r.commit()
+        >>> r.check_call(['git','rev-parse', 'HEAD'])
+        0
+        '''
+        self.check_call(['git','commit', '-m', msg])
+
     def check_call(self, *args, **kw):
-        return quiet_process.check_call(
+        '''
+        >>> LocalGit().check_call(['git', 'status'])
+        0
+        '''
+        return quietly.check_call(
             *args, **dict(kw, cwd=kw.get('cwd',self.path)))
 
 if __name__ == '__main__':
