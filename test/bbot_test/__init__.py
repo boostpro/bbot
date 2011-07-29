fd22dc434a7362deba284416b8560f1d2cf42937
Dave Abrahams <dave@boostpro.com>
2011-07-29 04:11:33 -0400

    bbot parent directory should be inserted at the beginning of sys.path
    ...in case of previously-installed bbot

--- test/bbot_test/__init__.py
+++ test/bbot_test/__init__.py
@@ -3,7 +3,9 @@
 # project, but this test project is of course part of the bbot source
 # distribution, so the source for bbot itself is above here.
 #
-import sys, os
+import sys
 from os.path import dirname as pdir
-sys.path.append(pdir(pdir(pdir(pdir(__file__)))))
+_bbot_parent_dir = pdir(pdir(pdir(pdir(__file__))))
+if _bbot_parent_dir not in sys.path:
+    sys.path.insert(0, _bbot_parent_dir)
 
