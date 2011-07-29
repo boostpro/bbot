cf24a17f97292fd482f1aeb294c7e48c9e2eef63
Dave Abrahams <dave@boostpro.com>
2011-07-29 00:52:43 -0400

    Clarify the code that gets bbot into the path of the test project

--- test/bbot_test/__init__.py
+++ test/bbot_test/__init__.py
@@ -1,6 +1,9 @@
-# Get bbot into the path
-import inspect, sys, os
-from os.path import dirname, join
-sys.path.append(
-    dirname( dirname( dirname( dirname(
-        os.path.join(os.getcwd(), inspect.stack()[0][1]))))))
+#
+# Get bbot into the path.  This part wouldn't be needed in an ordinary
+# project, but this test project is of course part of the bbot source
+# distribution, so the source for bbot itself is above here.
+#
+import sys, os
+from os.path import dirname as pdir
+sys.path.append(pdir(pdir(pdir(pdir(__file__)))))
+
