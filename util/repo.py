459b5a9d81e2fa5465de525dc42e743f740a5b6d
Dave Abrahams <dave@boostpro.com>
2011-07-30 18:44:37 -0400

    Set up the buildbot change hook

--- util/repo.py
+++ util/repo.py
@@ -1,5 +1,8 @@
-import tempdir
 import quiet_process as quietly
+import shell
+from path import Path
+import tempdir
+import os
 
 def _nonempty_repo(add=True,commit=None):
     r = LocalGit()
@@ -11,13 +14,25 @@ def _nonempty_repo(add=True,commit=None):
     return r
 
 class LocalGit(tempdir.TempDir):
-    def __init__(self, *args, **kw):
+
+    def __new__ (cls, bare=False, clone=None, *args, **kw):
+        if bare:
+            kw['suffix'] = '.git'
+        return super(LocalGit, cls).__new__(cls, *args, **kw)
+
+    def __init__(self, bare=False, clone=None, *args, **kw):
         '''
         >>> git = LocalGit()
         >>> quietly.check_call(['git','status'], cwd=git.path)
+        >>> clone = LocalGit(clone=git)
+        >>> quietly.check_call(['git','status'], cwd=clone.path)
         '''
         super(LocalGit, self).__init__(*args, **kw)
-        self.check_call(['init'])
+        options = bare and ['--bare'] or []
+        if clone:
+            self.check_call(['clone'] + options + [clone, self.name], cwd = self.folder)
+        else:
+            self.check_call(['init'] + options)
 
     def rev_parse(self, rev='HEAD'):
         '''
@@ -55,7 +70,41 @@ class LocalGit(tempdir.TempDir):
         '''
         return quietly.check_call( ['git']+args[0],
             *args[1:], **dict(kw, cwd=kw.get('cwd',self.path)))
+    
+    @property
+    def dot_git(self):
+        return self if self.endswith('.git') else self/'.git'
 
+    def install_post_receive_hook(self, **kw):
+        r'''
+        >>> remote = LocalGit(bare=True)
+        >>> remote.preserve()
+        >>> hook = remote.install_post_receive_hook()
+        >>> cont = open(hook).read()
+        >>> open(hook, 'a').write('set -e\n' + cont + 'echo done > ' + shell.quote(remote/'done'))
+        >>> local = _nonempty_repo()
+        >>> local.check_call(['remote', 'add', 'origin', remote])
+        >>> local.check_call(['push', 'origin', 'master:foo'], cwd=local)
+        >>> assert (remote/'done').exists
+        '''
+        hookpath = self.dot_git/'hooks'/'post-receive'
+        hook = open(hookpath, 'w')
+
+        buildbot_src = Path(__file__).folder.folder/'external'/'buildbot'
+        script = buildbot_src/'master'/'contrib'/'git_buildbot.py'
+        kw['repository'] = kw.get('repository', self)
+
+        cmd = [ script, '-v' ]
+        for k,v in kw.items():
+            cmd += [ '--' + k, v]
+
+        hook.write(shell.list2cmdline(cmd) + ' || exit 1\n')
+        hook.close()
+        os.lchmod(hookpath, 0777)
+        return hookpath
+
+        
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+
